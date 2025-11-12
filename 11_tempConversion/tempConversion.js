c0b880e7798af11ddc55f101537698a294612cc4
Soljira <soljiragit@gmail.com>
2025-11-12 10:06:34 +0800

    feat: finish 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+function roundUpToOneDecimal(num) {
+    return Math.round(num * 10) / 10;
+}
+
+const convertToCelsius = function (temp) {
+    return roundUpToOneDecimal((temp - 32) / 1.8);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    return roundUpToOneDecimal((9 / 5) * temp + 32);
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
