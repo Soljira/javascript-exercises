1f2c6d1f9233478940af4db0fe7e4d313215dd1a
Soljira <soljiragit@gmail.com>
2025-11-12 09:09:12 +0800

    feat: finish 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, itemToBeRemoved) {
+    // while (arr.includes(itemToBeRemoved)) {
+    //     let index = arr.findIndex((item) => item == itemToBeRemoved);
+    //     arr.splice(index, 1);
+    // }
+
+    // Optional arguments
+    const args = [...arguments];
+    return arr.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
