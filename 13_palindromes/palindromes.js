687b4abe2c8e9dbfed343645fb55327fabab11db
Soljira <soljiragit@gmail.com>
2025-12-04 15:31:57 +0800

    feat: finish 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+/*
+    1. Split only alphanumeric characters; store this as origArr
+    2. Reverse the origArr and store it to reversedArr
+    3. In a loop, compare each index to see if it matches
 
+*/
+
+const palindromes = function (string) {
+    let cleanedString = string.replace(/[^a-zA-Z0-9]/g, "");
+    let origArr = cleanedString.split("");
+    let reversedArr = [...origArr].reverse();
+
+    let isPalindrome = false;
+
+    for (let character = 0; character < origArr.length; character++) {
+        if (
+            origArr[character].toLowerCase() !=
+            reversedArr[character].toLowerCase()
+        ) {
+            return false;
+        } else {
+            isPalindrome = true;
+        }
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
