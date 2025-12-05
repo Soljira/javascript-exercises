ce0f514c487dc8b4f29305c36561bff994d563f7
Soljira <soljiragit@gmail.com>
2025-12-05 17:49:56 +0800

    feat: finish 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    let newArr = arr.sort((a, b) => {
+        if ("yearOfDeath" in a && "yearOfDeath" in b) {
+            return (
+                a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth)
+            );
+        } else {
+            const currentYear = new Date().getFullYear();
+            if (!("yearOfDeath" in a) && "yearOfDeath" in b) {
+                return (
+                    currentYear -
+                    a.yearOfBirth -
+                    (b.yearOfDeath - b.yearOfBirth)
+                );
+            } else if (!("yearOfDeath" in b) && "yearOfDeath" in a) {
+                return (
+                    a.yearOfDeath -
+                    a.yearOfBirth -
+                    (currentYear - b.yearOfBirth)
+                );
+            }
+        }
+    });
 
+    return newArr.at(-1);
 };
 
 // Do not edit below this line
